25941b023709dcbca7a3d857d0900050a2fba8d0
Willian Ferreira <williannfferreira@gmail.com>
2019-05-07 09:46:25 -0300

    Finalizando por enquanto

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-paragrafo = document.querySelector('#text');
-
-texto = 'Github Pages Exemplo - novo';
-
-texto = texto.split('');
-
-for(let i = 0; i < texto.length; i++) {
-  setTimeout(() => {
-    paragrafo.innerText += texto[i];
-  }, i * 70);
+function typeWriter(element) {
+  const textArray = element.innerHTML.split('');
+  element.innerHTML = '';
+  textArray.forEach((letter, i) => {
+    setTimeout(() => {
+      element.innerHTML += letter;
+    }, i * 75);
+  });
 }
+
+const p = document.querySelector('#text');
+typeWriter(p);
