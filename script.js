f4b6f19b0e0bdd7159b1d64be5083dd98be1035f
Willian Ferreira <33581079+williannferreira@users.noreply.github.com>
2019-05-07 22:29:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,19 +4,32 @@ function typeWriter(element) {
   textArray.forEach((letter, i) => {
     setTimeout(() => element.innerHTML += letter, i * 75);
   });
-  // blinkText(element);
+  blinkText(element);
 }
 
 function blinkText(element) {
+  let backgroundColor = getRandomColor();
+  let color = getRandomColor();
   setInterval(() => {
-    if (element.style.backgroundColor == 'black') {
-      element.style.backgroundColor = 'white';
-      element.style.color = 'black';
-    } else {
-      element.style.backgroundColor = 'black';
-      element.style.color = 'white';
-    }
+    element.style.backgroundColor = backgroundColor;
+    element.style.color = color;
+//     if (element.style.backgroundColor == 'black') {
+//       element.style.backgroundColor = 'white';
+//       element.style.color = 'black';
+//     } else {
+//       element.style.backgroundColor = 'black';
+//       element.style.color = 'white';
+//     }
   }, 500)
+}
+
+function getRandomColor() {
+  let letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
 
 const p = document.querySelector('#text');
