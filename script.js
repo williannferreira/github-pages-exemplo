b63f2f6f4128351defec4fafb224e19553dd4775
Willian Ferreira <33581079+williannferreira@users.noreply.github.com>
2019-05-07 22:21:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,19 @@ function typeWriter(element) {
       element.innerHTML += letter;
     }, i * 75);
   });
+  blinkText(element);
+}
+
+function blinkText(element) {
+  setInterval(() => {
+    if (element.style.backgroundColor == 'black') {
+      element.style.backgroundColor = 'white';
+      element.style.color = 'black';
+    } else {
+      element.style.backgroundColor = 'black';
+      element.style.color = 'white';
+    }
+  }, 2500)
 }
 
 const p = document.querySelector('#text');
